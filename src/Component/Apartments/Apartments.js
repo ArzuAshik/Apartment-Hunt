80f5051ad024d6d741bb9cedeee3f834e94b6e5c
Masfik Alam <masfikalamfrp@gmail.com>
2020-11-17 19:34:12 +0600

    code improved

--- src/Component/Apartments/Apartments.js
+++ src/Component/Apartments/Apartments.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './Apartments.css';
-import bed from '../../images/bed.png';
-import bath from '../../images/bath.png';
-import map from '../../images/map.png';
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import SearchResult from './SearchResult';
